d849076c829dadd266b1517c41487ea8b62bb86d
BunnyBlack <xyy6836@126.com>
2021-07-05 15:25:17 +0800

    Init commit

---
+++ Assets/Scripts/Core/GameRoot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace Core
+{
+    public class GameRoot : MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log("Game Start...");
+        }
+    }
+}
